206d46ebbf9e4202e1dc950ada94fea02e376e53
ben wanicur <bwanicur@gmail.com>
2015-05-13 09:23:50 -0700

    ok, disabling devise is not going to work

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
 
-  # devise :database_authenticatable, :registerable, :confirmable, :recoverable, :rememberable, :trackable, :validatable
+  devise :database_authenticatable, :registerable, :confirmable, :recoverable, :rememberable, :trackable, :validatable
 
 
 end
