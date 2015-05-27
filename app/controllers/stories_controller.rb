7bad24d22819df5e341b4fbebe378ee997b0b853
greggehrich <ggehrich@gmail.com>
2015-05-26 22:13:24 -0700

    dynamic gmap pins

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -5,10 +5,19 @@ class StoriesController < ApplicationController
     page = params[:page] ? params[:page] : 1
     @stories = Story.ready_for_display.paginate(per_page: 50, page: page).order('created_at DESC')
     @stories = @stories.where(["title ILIKE ?", "%#{params[:search]}%"]) if params[:search]
+
   end
 
   def show
     @story = Story.find(params[:id])
+    @places = @story.places
+    
+    # the hash holds all the markers for the map
+    @hash = Gmaps4rails.build_markers(@places) do |place, marker|
+      marker.lat place.location.lat
+      marker.lng place.location.lng
+    end
+
   end
 
 end
