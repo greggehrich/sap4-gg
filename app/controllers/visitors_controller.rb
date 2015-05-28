dbcdb89569deb3f041f40c35edc3e2a1605d0d7f
greggehrich <ggehrich@gmail.com>
2015-05-28 11:23:44 -0700

    prod proto story index

--- app/controllers/visitors_controller.rb
+++ app/controllers/visitors_controller.rb
@@ -1,7 +1,7 @@
 class VisitorsController < ApplicationController
 
   def index
-    @stories = Story.order("id DESC").first(10)
+    @stories = Story.order("id DESC").first(21)
     # @stories = Story.order("id DESC").all.includes(:urls)
   end
 end
