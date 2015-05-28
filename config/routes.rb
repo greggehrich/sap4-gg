809ba22ba6b1793bec11788e5a0f0ff05e669d48
greggehrich <ggehrich@gmail.com>
2015-05-27 22:58:00 -0700

    new maps page

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   resources :places
 
   match 'stories/:id/places' => 'stories#story_places_list', via: [:get]
-  match 'stories/:id/places/map' => 'stories#story_places_map', via: [:get]
+  match 'stories/:id/places/map' => 'stories#story_places_map', via: [:get], as: 'story_places_map'
 
   resources :stories
 
