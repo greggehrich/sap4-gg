8545beeb6e74735b40bead3c09510afca1542509
greggehrich <ggehrich@gmail.com>
2015-06-04 17:28:43 -0700

    add user acct and auth

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,20 +1,20 @@
 class UsersController < ApplicationController
-  before_filter :authenticate_user!
-  after_action :verify_authorized
+  # before_filter :authenticate_user!
+  # after_action :verify_authorized
 
   def index
     @users = User.all
-    authorize User
+    # authorize User
   end
 
   def show
     @user = User.find(params[:id])
-    authorize @user
+    # authorize @user
   end
 
   def update
     @user = User.find(params[:id])
-    authorize @user
+    # authorize @user
     if @user.update_attributes(secure_params)
       redirect_to users_path, :notice => "User updated."
     else
