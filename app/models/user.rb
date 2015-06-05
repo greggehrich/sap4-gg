137b191ba6c5bcdcac15c3d26cd8d42a44b80745
greggehrich <ggehrich@gmail.com>
2015-06-04 22:27:02 -0700

    add user acct and auth

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,7 @@ class User < ActiveRecord::Base
 
   devise :database_authenticatable, :registerable, :confirmable, :recoverable, :rememberable, :trackable, :validatable
 
-  enum role: [:user, :vip, :admin]
+  enum role: [:user, :associate, :admin]
   after_initialize :set_default_role, :if => :new_record?
 
   def set_default_role
