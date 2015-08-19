df696f4ae9dcf373b597692511bf2034b167d4d9
greggehrich <ggehrich@gmail.com>
2015-08-19 15:51:46 -0700

    place, location edit

--- app/models/image.rb
+++ app/models/image.rb
@@ -1,6 +1,10 @@
 class Image < ActiveRecord::Base
 
   validates :image_type, presence: true
+  validates :image_type, :presence => { :message => "IMAGE TYPE is required" }
+  validates :image_size_h, :presence => { :message => "HORIZONTAL PX SIZE is required" }
+  validates :image_size_v, :presence => { :message => "VERICAL PX SIZE is required" }
+  validates :source, :presence => { :message => "SOURCE is required" }
 
   has_one :url, as: :urlable, dependent: :destroy
   accepts_nested_attributes_for :url
